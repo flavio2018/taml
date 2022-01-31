26217758b712f5c2a72f9fa85256313887653086
flavio2018 <flavio.petruzzellis@gmail.com>
2022-01-31 16:27:25 +0100

    Create v1 of data files generator script

--- src/data/generate_data_folder.py
+++ src/data/generate_data_folder.py
@@ -8,13 +8,17 @@ from src.data.batch_generators import generate_input_batch, generate_output_batc
 
 
 @click.command()
-@click.argument('output_filepath', type=click.Path())
-@click.argument('num_batches', type=int)
+@click.option('--output_filepath', type=click.Path())
+@click.option('--num_batches', type=int)
 @Timer(text=lambda secs: f"Took {format_timespan(secs)}")
-def main(output_file, num_batches):
-    for __ in range(num_batches):
-        input_batch = generate_input_batch()
-        output_batch = generate_output_batch()
+def main(output_filepath, num_batches):
+    with open(output_filepath, "w+") as input_data_file:
+        for __ in range(num_batches):
+            input_batch = generate_input_batch()
+            output_batch = generate_output_batch(input_batch)
+            for sample in output_batch:
+                input_data_file.write(" ".join([str(digit) for digit in sample]))  # sample is an array of 59 digits
+                input_data_file.write("\n")
 
 
 if __name__ == "__main__":
