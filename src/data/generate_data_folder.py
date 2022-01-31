c1f6d07a352b0f8f79d509452ff626408a781de2
flavio2018 <flavio.petruzzellis@gmail.com>
2022-01-31 16:58:04 +0100

    Include inputs in data generating script

--- src/data/generate_data_folder.py
+++ src/data/generate_data_folder.py
@@ -8,17 +8,26 @@ from src.data.batch_generators import generate_input_batch, generate_output_batc
 
 
 @click.command()
-@click.option('--output_filepath', type=click.Path())
+@click.option('--inputs_filepath', type=click.Path())
+@click.option('--targets_filepath', type=click.Path())
 @click.option('--num_batches', type=int)
 @Timer(text=lambda secs: f"Took {format_timespan(secs)}")
-def main(output_filepath, num_batches):
-    with open(output_filepath, "w+") as input_data_file:
-        for __ in range(num_batches):
-            input_batch = generate_input_batch()
-            output_batch = generate_output_batch(input_batch)
-            for sample in output_batch:
-                input_data_file.write(" ".join([str(digit) for digit in sample]))  # sample is an array of 59 digits
-                input_data_file.write("\n")
+def main(inputs_filepath, targets_filepath, num_batches):
+    for __ in range(num_batches):
+        input_batch = generate_input_batch()
+        output_batch = generate_output_batch(input_batch)
+
+        with open(inputs_filepath, "w+") as inputs_data_file:
+            _write_batch(input_batch, inputs_data_file)
+
+        with open(targets_filepath, "w+") as targets_data_file:
+            _write_batch(output_batch, targets_data_file)
+
+
+def _write_batch(batch, file):
+    for sample in batch:
+        file.write(" ".join([str(digit) for digit in sample]))  # sample is an array of 59 digits
+        file.write("\n")
 
 
 if __name__ == "__main__":
