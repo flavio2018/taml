2187c27fd4ae6b03f37fc1e86ee8287342e62e2f
flavio2018 <flavio.petruzzellis@gmail.com>
2022-01-31 16:26:44 +0100

    Finalize and docstring labels one-hot encoding

--- src/data/data_ops.py
+++ src/data/data_ops.py
@@ -11,9 +11,12 @@ def pad_output_batch(output_batch):
 
 
 def one_hot_encode_output(output_batch):
+    """Transform a batch of outputs with categorical labels into a batch with one-hot encoded labels."""
     lb = LabelBinarizer()
     lb.fit(list(range(19)))  # la somma 1 non può esserci
     new_output_batch = lb.transform(output_batch[0])
     for i in range(1, output_batch.shape[0]):
-        new_output_batch = np.stack([new_output_batch, lb.transform(output_batch[i])])
-    return new_output_batch
+        one_hot_encoded_labels = lb.transform(output_batch[i])
+        # print(np.shape(new_output_batch))
+        new_output_batch = np.concatenate([new_output_batch, one_hot_encoded_labels])
+    return np.reshape(new_output_batch, (-1, 60, 19))
