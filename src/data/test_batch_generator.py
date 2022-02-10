9de3a4fcafd814f60f5c0dad8316d6d462d3f8c1
flavio2018 <flavio.petruzzellis@gmail.com>
2022-02-10 14:44:15 +0100

    Automatic change function name

--- src/data/test_batch_generator.py
+++ src/data/test_batch_generator.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from batch_generators import generate_input_batch, generate_output_batch
+from batch_generators import generate_sum_input_batch, generate_sum_output_batch
 
 
 def main():
@@ -10,19 +10,19 @@ def main():
         
 
 def _test_input_batch_shape():
-    input_batch = generate_input_batch(batch_size=300)
+    input_batch = generate_sum_input_batch(batch_size=300)
     assert(input_batch.shape == (300, 60))
 
 
 def _test_output_batch_shape():
     ones_matrix = np.ones((10, 60))
-    output_batch = generate_output_batch(ones_matrix)
+    output_batch = generate_sum_output_batch(ones_matrix)
     assert(output_batch.shape == (10, 59))
 
 
 def _test_sums_in_output_batch():
     ones_matrix = np.ones((10, 60))
-    output_batch = generate_output_batch(ones_matrix)
+    output_batch = generate_sum_output_batch(ones_matrix)
     all_elements_are_twos = (output_batch == 2).all()
     assert(all_elements_are_twos == True)
 
