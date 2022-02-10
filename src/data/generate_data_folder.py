276562b14ef9e3a4a8e45a87ee4b77344a07dbc8
flavio2018 <flavio.petruzzellis@gmail.com>
2022-02-10 15:39:28 +0100

    Update data folder creator
    - use append mode
    - change range param

--- src/data/generate_data_folder.py
+++ src/data/generate_data_folder.py
@@ -11,23 +11,27 @@ from src.data.batch_generators import generate_discriminate_input_batch, generat
 @click.option('--inputs_filepath', type=click.Path())
 @click.option('--targets_filepath', type=click.Path())
 @click.option('--num_batches', type=int)
-@click.option('--n_range', type=tuple, default=(1, 50))
+@click.option('--range_start', type=int, default=1)
+@click.option('--range_end', type=int, default=50)
 @click.option('--only_int', type=bool)
 @click.option('--only_even', type=bool)
 @Timer(text=lambda secs: f"Took {format_timespan(secs)}")
-def main(inputs_filepath, targets_filepath, num_batches, n_range, only_int, only_even):
+def main(inputs_filepath, targets_filepath, num_batches, range_start, range_end, only_int, only_even):
     if only_even:
-        only_these = [n for n in range(*n_range) if n % 2 == 0]
+        only_these = [n for n in range(range_start, range_end) if n % 2 == 0]
     else:
         only_these = None
+
     for __ in range(num_batches):
-        input_batch = generate_discriminate_input_batch(range=n_range, only_int=only_int, only_these=only_these)
+        input_batch = generate_discriminate_input_batch(range=(range_start, range_end),
+                                                        only_int=only_int,
+                                                        only_these=only_these)
         output_batch = generate_discriminate_output_batch(input_batch)
 
-        with open(inputs_filepath, "w+") as inputs_data_file:
+        with open(inputs_filepath, "a+") as inputs_data_file:
             _write_batch(input_batch, inputs_data_file)
 
-        with open(targets_filepath, "w+") as targets_data_file:
+        with open(targets_filepath, "a+") as targets_data_file:
             for el in output_batch:
                 targets_data_file.write(str(el) + "\n")
 
